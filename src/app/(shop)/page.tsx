fc5d7d9ced9fc16881b51ad3e9c2596e110e4822
mestrada <mestrada@gmail.com>
2024-11-11 15:32:13 +0100

    Folder structure and directories

---
+++ src/app/(shop)/page.tsx
@@ -0,0 +1,10 @@
+import { titleFont } from "@/config/fonts";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Hola mundo</h1>
+      <h1 className={`${titleFont.className} font-bold`}>Hola mundo</h1>
+    </div>
+  )
+}
---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>New account</h1>
+    </div>
+  );
+}

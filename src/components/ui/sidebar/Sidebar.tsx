343c5030e6e2995fba286f0c3c348af76827dd53
mestrada <mestrada@gmail.com>
2024-11-12 18:45:20 +0100

    Sidebar close menu onClick outside

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -25,6 +25,7 @@ export const Sidebar = () => {
             {
                 isSideMenuOpen && (
                     <div
+                        onClick={ () => closeMenu() }
                         className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm"
                     />
                 )

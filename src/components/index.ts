ff90fa934c9e401783f556adcb4a89446b2d86f8
mestrada <mestrada@gmail.com>
2024-11-13 19:32:07 +0100

    Footer

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export * from './product/slideshow/ProductMobileSlideShow';
 export * from './product/slideshow/ProductSlideShow';
 export * from './products/product-grid/ProductGrid';
 export * from './products/product-grid/ProductGridItem';
+export * from './ui/footer/Footer';
 export * from './ui/not-found/PageNotFound';
 export * from './ui/sidebar/Sidebar';
 export * from './ui/sidebar/SidebarItem';

a3921331ea1c9226ad9981414c5e89bf50ab48a5
mestrada <mestrada@gmail.com>
2024-11-19 20:10:16 +0100

    Login button disabled when isPending response

--- src/app/auth/login/ui/LoginForm.tsx
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { authenticate } from '@/actions';
+import clsx from 'clsx';
 import Link from 'next/link';
 import { useActionState } from 'react';
+import { IoInformationOutline } from 'react-icons/io5';
 
 export const LoginForm = () => {
   const [errorMessage, formAction, isPending] = useActionState(
@@ -26,7 +28,27 @@ export const LoginForm = () => {
         name="password"
       />
 
-      <button type="submit" className="btn-primary">
+      <div
+        className="flex h-8 items-end space-x-1"
+        aria-live="polite"
+        aria-atomic="true"
+      >
+        {errorMessage && (
+          <div className="flex flex-row mb-2">
+            <IoInformationOutline className="h-5 w-5 text-red-500" />
+            <p className="text-sm text-red-500">{errorMessage}</p>
+          </div>
+        )}
+      </div>
+
+      <button
+        type="submit"
+        className={clsx({
+          'btn-primary': !isPending,
+          'btn-disabled': isPending,
+        })}
+        disabled={isPending}
+      >
         Log in
       </button>
 

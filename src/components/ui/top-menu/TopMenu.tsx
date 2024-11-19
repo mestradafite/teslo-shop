efae5b8429ef4fffb3a24018629fa3919c3353e8
mestrada <mestrada@gmail.com>
2024-11-19 18:19:51 +0100

    Empty cart and USD formated

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -57,10 +57,14 @@ export const TopMenu = () => {
           <IoSearchOutline className="w-5 h-5" />
         </Link>
 
-        <Link className="mx-2" href="/cart">
+        <Link className="mx-2" href={
+          ((totalItemsInCart === 0) && loaded)
+          ? "/empty"
+          : "/cart"
+        }>
           <div className="relative">
             {(loaded && totalItemsInCart > 0) && (
-              <span className="absolute text-xs px-1 rounded-full font-bold -top-2 -right-2 bg-blue-700 text-white">
+              <span className="fade-in absolute text-xs px-1 rounded-full font-bold -top-2 -right-2 bg-blue-700 text-white">
                 {totalItemsInCart}
               </span>
             )}

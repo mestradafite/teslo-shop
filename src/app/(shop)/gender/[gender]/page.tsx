c1f6a0d78bfe8808d53d2540a503d9c8e455cfa2
mestrada <mestrada@gmail.com>
2024-11-26 22:21:44 +0100

    Texts reafactor

--- src/app/(shop)/gender/[gender]/page.tsx
+++ src/app/(shop)/gender/[gender]/page.tsx
@@ -7,53 +7,45 @@ import { Gender } from '@prisma/client';
 import { redirect } from 'next/navigation';
 
 interface Props {
-  params: Promise<{ gender: string }>
-  searchParams: Promise<{ page?: string }>
+  params: Promise<{ gender: string }>;
+  searchParams: Promise<{ page?: string }>;
 }
 
-
 export default async function GenderByPage({ params, searchParams }: Props) {
-
   const { gender } = await params;
   const page: number = Number((await searchParams).page) ?? 1;
 
-  const { products, totalPages } = await getPaginatedProductsWithImages({ 
-    page, 
+  const { products, totalPages } = await getPaginatedProductsWithImages({
+    page,
     gender: gender as Gender,
   });
 
-
-  if ( products.length === 0 ) {
-    redirect(`/gender/${ gender }`);
+  if (products.length === 0) {
+    redirect(`/gender/${gender}`);
   }
-  
 
-  const labels: Record<string, string>  = {
-    'men': 'para hombres',
-    'women': 'para mujeres',
-    'kid': 'para niños',
-    'unisex': 'para todos'
-  }
+  const labels: Record<string, string> = {
+    men: 'Men',
+    women: 'Women',
+    kid: 'Kids',
+    unisex: 'All',
+  };
 
   // if ( id === 'kids' ) {
   //   notFound();
   // }
 
-
   return (
     <>
       <Title
-        title={`Artículos de ${ labels[gender] }`}
-        subtitle="Todos los productos"
+        title={`${labels[gender]}`}
+        subtitle="Best Sellers"
         className="mb-2"
       />
 
-      <ProductGrid 
-        products={ products }
-      />
+      <ProductGrid products={products} />
 
-      <Pagination totalPages={ totalPages }  />
-      
+      <Pagination totalPages={totalPages} />
     </>
   );
 }
